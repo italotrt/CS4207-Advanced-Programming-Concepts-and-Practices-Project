f84004c55ae232db2d5af099f87b339620089869
Milan <kmilanm13@gmail.com>
2024-12-07 23:27:27 +0000

    [Milan] It all makes sense now, now I know what I am doing

--- project/contracts/Chat.sol
+++ project/contracts/Chat.sol
@@ -2,50 +2,50 @@
 pragma solidity ^0.8.0;
 
 contract Chat {
+    // Data structure for student
     struct Student {
         string studentId;
         string name;
     }
 
+    // Data structure for message
     struct Message {
         string message;
         string programmingLanguage;
         Student student;
     }
 
+    // Mappers
     mapping(string => Student) public students;
     mapping(string => bool) public existingStudents;
 
+    // Arrays
     string[] public studentIds;
     Message[] public messages;
-    
+
+    // Event emitters for frontend
     event StudentAdded(string studentId);
     event MessageAdded(string message, string programmingLanguage, string studentId);
 
+
     function addStudent(string memory name, string memory studentId) public payable {
+        // Validate the student exists within our blockchain
         require(!existingStudents[studentId], "Student ID already exists on the blockchain");
 
+        // Add student struct to the list
         students[studentId] = Student(studentId, name);
-        existingStudents[studentId] = true;
-        studentIds.push(studentId);
+        existingStudents[studentId] = true; // Hashmap to check if student exists
+        studentIds.push(studentId); // append
 
-        emit StudentAdded(studentId);
+        emit StudentAdded(studentId); // Emit the event
     }
 
     function sendMessage(string memory message, string memory programmingLanguage, string memory studentId) public payable {
+        // Validate student exists within our blockchain
         require(existingStudents[studentId], "Student does not exist!");
-        // require(students[studentId], "Student doesn't exists on the blockchain");
-        Student storage student = students[studentId];
-        messages.push(Message(message, programmingLanguage, student));
-        emit MessageAdded(message, programmingLanguage, studentId);
-    }
 
-    function getStudentIds() public view returns (Student[] memory) {
-        Student[] memory studentGet = new Student[](studentIds.length);
-        for (uint i = 0; i < studentIds.length; i++) {
-            studentGet[i] = students[studentIds[i]];
-        }
-        return studentGet;
+        Student storage student = students[studentId]; // Get the student
+        messages.push(Message(message, programmingLanguage, student)); // Append the message
+        emit MessageAdded(message, programmingLanguage, studentId); // Emit the event
     }
-
 }
